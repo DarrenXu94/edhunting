d759cfde2d3e75a451d4882f0ff35ed3b398847a
Darren <darrenxu1994@gmail.com>
2019-03-03 09:27:56 +1100

    fixed scrolling and added desktop about

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,17 @@
 function myFunction() {
-    document.getElementById("icons").classList.toggle("show");
+  document.getElementById("icons").classList.toggle("show");
+}
+
+function showContact() {
+  var x = document.getElementById("bio-wrapper-id");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-  
+}
+
+function showHome() {
+  var x = document.getElementById("bio-wrapper-id");
+  x.style.display = "none";
+}
